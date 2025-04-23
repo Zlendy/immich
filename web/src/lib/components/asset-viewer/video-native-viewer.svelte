1be40c9a46126b23cbfcf493690387146d5c5674
Zlendy <zlendy@proton.me>
2025-04-23 23:52:07 +0200

    feat: Incomplete implementation of custom <video> controls for "video-native-viewer.svelte"

--- web/src/lib/components/asset-viewer/video-native-viewer.svelte
+++ web/src/lib/components/asset-viewer/video-native-viewer.svelte
@@ -11,6 +11,9 @@
   import { t } from 'svelte-i18n';
   import { isFaceEditMode } from '$lib/stores/face-edit.svelte';
   import FaceEditor from '$lib/components/asset-viewer/face-editor/face-editor.svelte';
+  import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
+  import { mdiPause, mdiPlay, mdiFullscreen, mdiFullscreenExit } from '@mdi/js';
+  import { Duration } from 'luxon';
 
   interface Props {
     assetId: string;
@@ -34,11 +37,18 @@
     onClose = () => {},
   }: Props = $props();
 
+  let wrapper: HTMLDivElement | undefined = $state();
   let videoPlayer: HTMLVideoElement | undefined = $state();
   let isLoading = $state(true);
   let assetFileUrl = $state('');
   let forceMuted = $state(false);
   let isScrubbing = $state(false);
+  let currentTime = $state(0);
+  let duration = $state(0);
+  let paused = $state(false);
+  let isFullscreen = $state(false);
+
+  const videoPlayerVolumePercent = $derived(Math.floor($videoViewerVolume * 100));
 
   onMount(() => {
     if (videoPlayer) {
@@ -98,9 +108,16 @@
       videoPlayer?.pause();
     }
   });
+
+  const formatSeconds = (seconds: number) => {
+    if (Number.isNaN(seconds)) seconds = 0;
+    const duration = Duration.fromObject({ seconds });
+    return duration.toFormat(duration.hours > 0 ? 'hh:mm:ss' : 'mm:ss');
+  };
 </script>
 
 <div
+  bind:this={wrapper}
   transition:fade={{ duration: 150 }}
   class="flex h-full select-none place-content-center place-items-center"
   bind:clientWidth={containerWidth}
@@ -132,8 +149,34 @@
     bind:volume={$videoViewerVolume}
     poster={getAssetThumbnailUrl({ id: assetId, size: AssetMediaSize.Preview, cacheKey })}
     src={assetFileUrl}
+    bind:currentTime
+    bind:duration
+    bind:paused
   >
   </video>
+
+  <div
+    class="absolute bottom-4 text-immich-fg bg-immich-bg dark:bg-immich-dark-bg dark:text-immich-dark-fg rounded-xl px-4 py-2 flex gap-4 align-middle"
+  >
+    <CircleIconButton title="TODO" icon={paused ? mdiPlay : mdiPause} onclick={() => (paused = !paused)} />
+
+    <span class="w-48 text-right self-center">{formatSeconds(currentTime)} / {formatSeconds(duration)}</span>
+    <input type="range" bind:value={currentTime} max={duration} step={0.01} />
+
+    <span class="w-12 text-right self-center">{videoPlayerVolumePercent}%</span>
+    <input type="range" bind:value={$videoViewerVolume} max={1} step={0.01} />
+
+    <CircleIconButton
+      title="TODO"
+      icon={isFullscreen ? mdiFullscreenExit : mdiFullscreen}
+      onclick={async () => {
+        if (isFullscreen) await document.exitFullscreen();
+        else await wrapper?.requestFullscreen();
+
+        isFullscreen = !isFullscreen;
+      }}
+    />
+  </div>
 
   {#if isLoading}
     <div class="absolute flex place-content-center place-items-center">
